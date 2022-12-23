1ddac704a0f785751e73c32f312e61d62a051de6
Ishac79 <56246816+Ishac79@users.noreply.github.com>
2022-12-23 12:39:13 +0000

    First version with Buefy and Bootstrap-Vue tables

---
+++ tables/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <section>
+      <b-tabs>
+        <b-tab-item label="Buefy Table">
+          <BuefyTable/>
+        </b-tab-item>
+        <b-tab-item label="Bootstrap-vue Table">
+          <BootstrapVueTable/>
+        </b-tab-item>
+      </b-tabs>
+    </section>
+</div>
+</template>
+
+<script>
+import BuefyTable from './components/buefyTable.vue';
+import BootstrapVueTable from './components/bootstrapVueTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BuefyTable,
+    BootstrapVueTable
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ tables/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import { TablePlugin } from 'bootstrap-vue';
+
+Vue.use(TablePlugin);
+Vue.use(Buefy)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
